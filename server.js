a738563c0afdb5d7e7bf329e43361f922f822c3f
Muhammad Akhtar <105409960+M-Dan-Akhtar@users.noreply.github.com>
2023-08-01 22:10:22 -0400

    env changes

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const oneDay = 1000 * 60 * 60 * 24;
 
 //session middleware
 app.use(sessions({
-    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+    secret: process.env.SECRET_KEY,
     saveUninitialized:true,
     cookie: { maxAge: oneDay },
     resave: false
